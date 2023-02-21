070461c527d954df523c3b3cb9cc237975ae9d45
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2023-02-21 19:59:06 +0300

    v7

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.filters import SearchFilter 
+from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
