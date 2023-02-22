d058a013eeabbb55c21f118c917f3f989f5e0b18
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2023-02-22 11:39:22 +0300

    s5

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -142,10 +142,11 @@ USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ('static',)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
